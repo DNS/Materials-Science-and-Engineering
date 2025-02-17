d53e080a98c0606ba7f5da06d1fc01c80f238604
DNS <37093+DNS@users.noreply.github.com>
2025-02-17 21:56:47 +0700

    A: Manupedia\links.txt

--- manupedia-miner.ps1
+++ manupedia-miner.ps1
@@ -13,8 +13,35 @@ $b = iwr https://www.open.edu/openlearn/pluginfile.php/3277669/tool_ocwmanage/ar
 '<img src="(.+?)" alt="" />'
 
 
-https://www.open.edu/openlearn/manupedia?filter=date/grid/all/all/all/all/all/all/all
+https://www.open.edu/openlearn/manupedia?filter=date/grid/all/all/all/all/all/all/all&page=0
 https://www.open.edu/openlearn/manupedia?filter=date/grid/all/all/all/all/all/all/all&page=8
+
+
+$a = iwr 'https://www.open.edu/openlearn/manupedia?filter=date/grid/all/all/all/all/all/all/all&page=0'
+$b = $a.Links.href | sls engineering-technology | select -Unique
+
+------------------------
+
+0..8 | %{ iwr "https://www.open.edu/openlearn/manupedia?filter=date/grid/all/all/all/all/all/all/all&page=$_" } | % Links | % href | sls engineering-technology | select -Unique | Add-Content Manupedia\links.txt
+
+$links = gc Manupedia\links.txt
+	
+$titles = $links | 
+	%{ $_ -replace 'https://www.open.edu/openlearn/science-maths-technology/engineering-technology/.+?/.+?/.+?/' } | 
+	%{ $_ -replace 'https://www.open.edu/openlearn/science-maths-technology/engineering-technology/.+?/.+?/' } | 
+	%{ $_ -replace 'https://www.open.edu/openlearn/science-maths-technology/engineering-technology/.+?/' } | 
+	%{ $_ -replace 'https://www.open.edu/openlearn/science-maths-technology/engineering-technology/' }
+
+for ($i=0; $i -lt $links.Count; $i++) {
+	iwr $links[$i] | % Content | Out-File "Manupedia\$($titles[$i]).html"
+}
+
+
+
+
+
+https://www.open.edu/openlearn/science-maths-technology/engineering-technology/
+
 
 
 
